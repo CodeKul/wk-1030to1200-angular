503fb72f85a73a3b2f3706066ffdafd810a513bf
aniruddha <aniruddha.kudalkar@gmail.com>
2018-09-30 10:50:21 +0530

    loops : studied loops

---
+++ ts-intro/basics.js
@@ -0,0 +1,18 @@
+var nm = 'android';
+function calc(num) { }
+calc(10);
+var fn = function (num) { };
+fn(10);
+var obj = {
+    10: 'android',
+    "true": {
+        45: 96
+    }
+};
+var objV2 = {
+    'android': 'android',
+    45: '9798'
+};
+var myVar = 'android';
+var myFn = function () { console.log('Hi ' + myVar); };
+var arrFn = function () { return console.log(" hi " + myVar + " "); };
