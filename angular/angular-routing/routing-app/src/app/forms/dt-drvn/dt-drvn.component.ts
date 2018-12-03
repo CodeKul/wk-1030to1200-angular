7ce222f1bf6af9cc8ebe2df697c17a8ece8d64db
aniruddha <aniruddha.kudalkar@gmail.com>
2018-12-03 10:29:09 +0530

    data driven forms : custom and async validation

--- angular/angular-routing/routing-app/src/app/forms/dt-drvn/dt-drvn.component.ts
+++ angular/angular-routing/routing-app/src/app/forms/dt-drvn/dt-drvn.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dt-drvn',
@@ -8,24 +9,50 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class DtDrvnComponent implements OnInit {
 
-  fg : FormGroup
+  fg: FormGroup
 
   constructor(
-    private fb : FormBuilder
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     // web call for pulling data from server in the case of EDIT
 
     this.fg = this.fb.group({
-      usNm : this.fb.control('', Validators.required),
-      pass : ['', Validators.required],
-      eml : ['', Validators.compose([Validators.required, Validators.email]) ]
+      usNm: this.fb.control('', Validators.compose([Validators.required, this.startFromA])),
+      pass: ['', Validators.required, this.checkUsNm],
+      eml: ['', Validators.compose([Validators.required, Validators.email])]
     })
   }
 
-  mySub(){
+  mySub() {
     console.log(this.fg)
     console.log(this.fg.value)
+  }
+
+  startFromA(ctrl: AbstractControl): ValidationErrors | null {
+    if (ctrl.value.charAt(0) === 'A') {
+      return null
+    }
+    return {
+      isA: 'It should start from A'
+    }
+  }
+
+  checkUsNm(ctrl: AbstractControl): Observable<ValidationErrors | null> {
+    return Observable.create(
+      sub => {
+        setTimeout(() => {
+          if (ctrl.value === 'Android') {
+            sub.next(null)
+          } else {
+            sub.next({
+              isAndroid: 'Your user name should be Android'
+            })
+          }
+          sub.complete()
+        }, 2000)
+      }
+    )
   }
 }
